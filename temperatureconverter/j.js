ff4d63958617378cdcbce3226cc4aff82fe5f3b7
Kota Jahnavi <105514878+Jahnavikota@users.noreply.github.com>
2023-04-19 22:23:33 +0530

    Add files via upload

---
+++ temperatureconverter/j.js
@@ -0,0 +1,24 @@
+
+let celsius = document.getElementById('celsius');
+let fahrenheit = document.getElementById('fahrenheit');
+ 
+celsius.oninput = function () {
+    let f = (parseFloat(celsius.value) * 9) / 5 + 32;
+    fahrenheit.value = parseFloat(f.toFixed(2));
+   
+     
+}
+fahrenheit.oninput = function () {
+    let c = ((parseFloat(
+        fahrenheit.value) - 32) * 5) / 9;
+    celsius.value = parseFloat(c.toFixed(2));
+   
+     
+}
+
+document.getElementById('clear').onclick=clearForm;
+function clearForm()
+{
+    document.getElementById('fahrenheit').value='';
+    document.getElementById('celsius').value='';
+}
